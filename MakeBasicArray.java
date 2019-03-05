e16dadb50d4333470f45d1edec337f15927fb1eb
Joshua hernandez <43146943+Joshua01o@users.noreply.github.com>
2019-03-05 09:42:42 -0600

    Add files via upload

---
+++ MakeBasicArray.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package makebasicarray;
+/**
+ *
+ * @author joshua02o
+ */
+public class MakeBasicArray {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Joshua Hernandez";
+        char[] ary = name.toCharArray();
+        for (int i = ary.length-1; i >= 0; i--){        
+            if(i > 0){            
+                System.out.print(ary[i]+" ");}            
+            else {    
+                System.out.print(ary[i]);
+            for(int j = 0; j > 1000; j++){
+             
+                }
+            }
+           
+        }
+    }
+    
+}
